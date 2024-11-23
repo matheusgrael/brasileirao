e9201369d02e3fdcfffbe4e31a3ae380a2ee65a0
matheusgrael <matheusgrael@hotmail.com>
2024-11-23 13:30:17 +0000

    02: Classe Equipes criada

---
+++ Brasileirao/src/main/java/com/matheusgrael/Brasileirao/dominio/Equipes.java
@@ -0,0 +1,30 @@
+package com.matheusgrael.Brasileirao.dominio;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Equipes {
+
+    private UUID id;
+    private String nome;
+    private LocalDate anoFundacao;
+    private String Estado;
+    private int titulos;
+    private boolean serieA;
+
+    public int anosHistoria() {
+        return (LocalDate.now().getYear() - anoFundacao.getYear());
+    }
+
+
+}

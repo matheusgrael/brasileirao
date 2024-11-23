400e4943f6e4701cf9ab096f047f9f893f82629b
matheusgrael <matheusgrael@hotmail.com>
2024-11-23 12:00:05 +0000

    Initial commit

---
+++ Brasileirao/src/test/java/com/matheusgrael/Brasileirao/BrasileiraoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusgrael.Brasileirao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

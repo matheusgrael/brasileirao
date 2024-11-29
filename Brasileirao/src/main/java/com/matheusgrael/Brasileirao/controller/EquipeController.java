1af5501f96193e94d78cab79c8a99cc30572ccef
matheusgrael <matheusgrael@hotmail.com>
2024-11-29 10:43:44 +0000

    buscarUF

--- Brasileirao/src/main/java/com/matheusgrael/Brasileirao/controller/EquipeController.java
+++ Brasileirao/src/main/java/com/matheusgrael/Brasileirao/controller/EquipeController.java
@@ -39,6 +39,26 @@ public class EquipeController {
         return ResponseEntity.notFound().build();
     }
 
+    @GetMapping("/uf")
+//    @RequestParam?
+//    A anotação @RequestParam é usada para capturar valores enviados como parâmetros de consulta (query parameters) na URL.
+//    Por padrão, os parâmetros são obrigatórios, mas você pode configurá-los como opcionais com required = false.
+//Se o cliente faz uma requisição como://
+//    GET /equipe/uf?estado=RJ//
+//    Captura o valor do parâmetro estado ("RJ") e o associa à variável estado.
+//    Requisição sem incluir os parâmetros:
+//    GET /equipe/uf
+//    Como o required = false, estado será atribuído como null
+    public ResponseEntity<Map<UUID, Equipe>> buscarPorEstado(@RequestParam(required = false) String uf) {
+        Map<UUID, Equipe> equipes = equipeRepository.listAllEquipes();
+        if(uf != null) {
+            equipes.values().removeIf(e -> !e.getEstado().equalsIgnoreCase(uf));
+        }
+        return ResponseEntity.ok(equipes);
+    }
+
+
+
 
 
 

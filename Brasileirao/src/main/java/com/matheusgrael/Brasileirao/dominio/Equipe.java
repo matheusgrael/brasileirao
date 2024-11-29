967edff3cdc87048ce7716faec5f944de8563c5d
matheusgrael <matheusgrael@hotmail.com>
2024-11-29 15:41:47 +0000

    valids

--- Brasileirao/src/main/java/com/matheusgrael/Brasileirao/dominio/Equipe.java
+++ Brasileirao/src/main/java/com/matheusgrael/Brasileirao/dominio/Equipe.java
@@ -1,5 +1,8 @@
 package com.matheusgrael.Brasileirao.dominio;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,12 +17,15 @@ import java.util.UUID;
 public class Equipe {
 
     private UUID id;
-    @NotBlank
-
+    @NotBlank(message = "Nome é um requisito necessário")
     private String nome;
+    @NotNull(message = "Ano de fundação é um requisito necessário")
     private LocalDate anoFundacao;
+    @NotBlank(message = "Estado é um requisito necessário")
     private String Estado;
+    @Min(value = 0, message = "O número deve ser >= 0")
     private int titulosNacionais;
+    @Min(value = 0, message = "O número deve ser >= 0")
     private int titulosInternacionais;
     private boolean serieA;
 
